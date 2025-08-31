d3f10da128eaa6094daca9c01854e69fccaec830
Robert Lengberg <robert.lengberg@gmail.com>
2025-08-31 12:01:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+  # A python tool born out of convenience  
+  **Created by Robert Lengberg to process OKAB waybills for externally converted products.**<br/>
+
+  It will currently NOT handle scanned waybills (a.k.a OCR read)<br/>
+  OKAB can supply us with PDF files directly from their system<br/>
+  and this tool relies on them doing that.<br/>
+
+  Verify that the OKAB waybill is correct before relying on this tool<br/>
+  Verify the results of the CSV file before importing it in MEXIEC<br/>
+
+### Setup guide:
+>  1. Put the executable in its own folder on your PC<br/>
+>  2. Create a directory named "pdf"<br/>
+>  3. Create a directory named "csv"<br/>
+>  4. Remove old PDF files (the app will process all files in "pdf" folder)<br/>
+>  5. Place the PDF files you want to process in "pdf" folder.<br/>
+>   6. Run the executable, results will be output to "csv" folder.<br/>
+
+### Folder structure (top folder can be named anything)
+- OKAB Waybill Extractor
+  - /pdf
+  - /csv
+  - process_okab_waybills.exe
---
+++ process_okab_waybills.py
@@ -0,0 +1,137 @@
+import csv, pymupdf
+#from pprint import pprint
+from os.path import isfile, join
+from os import listdir
+
+def print_developed_by():
+	print("------------------------------------------------------------------------------")
+	print("-- Developed by Robert Lengberg in 2025 using the pyMuPDF library.          --")
+	print("-- Intended for use with OKAB waybills to import packages using MEXIEC.     --")
+	print("------------------------------------------------------------------------------")
+
+print_developed_by()
+
+src_path = "./pdf/"
+out_path = "./csv/"
+files = [f for f in listdir(src_path) if isfile(join(src_path, f))]
+
+for file in files:
+
+	document = pymupdf.open(src_path+file)
+	file_name = file[0:-4]
+	data = {"packages": {}, "consumed": {}, "header": {}}
+	i = 1
+
+	if (file[-4:] != '.pdf'):
+		print(f"\t{file} needs to have .pdf extension. Skipping...")
+		#continue # skip current iteration and jump to next
+
+	# This string is expected on the first page of the PDF.
+	# Jump to next iteration otherwise.
+	if document.is_pdf and "OKAB Sweden AB" in document.get_page_text(0):
+		print(f"\tProcessing {file}")
+	else:
+		print(f"\t{file} is not matching the expected PDF structure. Skipping...")
+		print(f"\tIf this is a waybill from OKAB, please verify that it is not a printed & scanned version.")
+		#continue #skip current iteration and jump to next
+
+	for page in document:
+		tabs = page.find_tables()
+
+		if len(tabs.tables) < 1:
+			continue
+
+		for tab in tabs.tables:
+
+			for l in tab.extract():
+
+				for s in l:
+
+					if s is None:
+						continue
+
+					# find order number and position
+					search = "Ordernummer (line)\n"
+					if search in s:
+						# extract the parts we want
+						order = s[len(search):-1].replace(" ", "").replace("(", "-").split("-")
+
+						# verify that we got exactly 2 parts and that they are both numeric.
+						if len(order) == 2 and order[0].isnumeric() and order[1].isnumeric():
+							data["header"]["order_no"] = order[0]
+							data["header"]["order_pos"] = int(order[1])
+							print(f"\t...found order position {data["header"]["order_no"]}-{data["header"]["order_pos"]}.")
+						else:
+							print(f"\t...order position with unknown format found {order}, skipping...")
+							continue
+
+
+					#find the tare weight per package
+					search = "Tara\n"
+					if search in s:
+						begin_at = len(search)
+						data["header"]["tare"] = s[begin_at:begin_at+len(s)-begin_at]
+						if len(data["header"]["tare"]) > 0:
+							print(f"\t...found tare weight: {data["header"]["tare"]} kg/package.")
+						else:
+							print(f"\t...no tare weight found in document, skipping...")
+
+						
+
+				# gather all new packages
+				if len(l) == 7 and l[0].isnumeric() and len(l[0]) == 14:
+					if len(data["packages"]) < 1:
+						print(f"\t...found packages to create.")
+					data["packages"][i] = {
+						"type": "P",
+						"id": l[0],
+						"sheets": l[3],
+						"gross_wt": l[4],
+						"net_wt": l[5],
+					}
+					i += 1
+
+				# gather all consumed packages
+				if len(l) == 6 and l[3].isnumeric() and len(l[3]) == 14:
+					if len(data["consumed"]) < 1:
+						print(f"\t...found source material to consume.")
+					data["consumed"][i] = {
+						"type": "I",
+						"id": l[3],
+						"new_length": 0
+					}
+					i += 1
+
+	if len(data["packages"]) > 0 and len(data["consumed"]) > 0 and len(data["header"]) == 3:
+
+		with open(out_path+str(data["header"]["order_no"])+"-"+str(data["header"]["order_pos"])+".csv", 'w', encoding='utf-8', newline='') as f:
+			csvw = csv.writer(f, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			
+			csvw.writerow([
+				'Type', 'Id', 'Order No', 'Order Pos', 'Net wt', 'Gross wt', 'Tare', 'Sheets'
+			])
+			for seq in data["packages"]:
+				csvw.writerow([
+					data["packages"][seq]["type"],
+					data["packages"][seq]["id"],
+					data["header"]["order_no"],
+					data["header"]["order_pos"],
+					data["packages"][seq]["net_wt"],
+					data["packages"][seq]["gross_wt"],
+					data["header"]["tare"],
+					data["packages"][seq]["sheets"],
+				])
+
+			for seq in data["consumed"]:
+				csvw.writerow([
+					data["consumed"][seq]["type"],
+					data["consumed"][seq]["id"],
+					data["consumed"][seq]["new_length"],
+
+				])
+
+		print(f"\t...finished processing {file}, output sent to {out_path[2:]}{file_name}.csv")
+	print(f"\n")
+
+print_developed_by()
+input("You can now close the window (press enter or use the X)")
